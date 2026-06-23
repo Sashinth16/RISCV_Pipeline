1419f7a62556c6fa3a5c439974f42af23848fd8a
Sashinth16 <sashinth5656@gmail.com>
2026-06-23 12:04:37 +0530

    Update control_unit.v

--- rtl/control_unit.v
+++ rtl/control_unit.v
@@ -27,18 +27,20 @@ module control_unit(
     output reg   mem_write,
     output reg   mem_to_reg,
     output reg   branch,
-    output reg [2:0] alu_op
+    output reg [2:0] alu_op,
+    output reg [2:0] imm_sel
 );
     always @(*) begin
         case(opcode)
-            7'b0110011: begin  // ADD/SUB
+            7'b0110011: begin  // ADD
                 reg_write  = 1;
                 alu_src    = 0;
                 mem_read   = 0;
                 mem_write  = 0;
                 mem_to_reg = 0;
                 branch     = 0;
-                alu_op     = 3'b010;
+                alu_op     = 3'b000;
+                imm_sel    = 3'b000;
             end
             7'b0010011: begin  // ADDI
                 reg_write  = 1;
@@ -47,7 +49,8 @@ module control_unit(
                 mem_write  = 0;
                 mem_to_reg = 0;
                 branch     = 0;
-                alu_op     = 3'b010;
+                alu_op     = 3'b000;
+                imm_sel    = 3'b000;
             end
             7'b0000011: begin  // LW
                 reg_write  = 1;
@@ -56,7 +59,8 @@ module control_unit(
                 mem_write  = 0;
                 mem_to_reg = 1;
                 branch     = 0;
-                alu_op     = 3'b010;
+                alu_op     = 3'b000;
+                imm_sel    = 3'b000;
             end
             7'b0100011: begin  // SW
                 reg_write  = 0;
@@ -65,7 +69,8 @@ module control_unit(
                 mem_write  = 1;
                 mem_to_reg = 0;
                 branch     = 0;
-                alu_op     = 3'b010;
+                alu_op     = 3'b000;
+                imm_sel    = 3'b001;
             end
             7'b1100011: begin  // BEQ
                 reg_write  = 0;
@@ -75,6 +80,7 @@ module control_unit(
                 mem_to_reg = 0;
                 branch     = 1;
                 alu_op     = 3'b001;
+                imm_sel    = 3'b010;
             end
             default: begin
                 reg_write  = 0;
@@ -84,6 +90,7 @@ module control_unit(
                 mem_to_reg = 0;
                 branch     = 0;
                 alu_op     = 3'b000;
+                imm_sel    = 3'b000;
             end
         endcase
     end
